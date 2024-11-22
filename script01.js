a510cb5ce697acfa9b00245cf9f14e00ac0019d5
MihPar <9496260@gmail.com>
2024-11-22 12:45:40 +0300

    next commit

--- script01.js
+++ script01.js
@@ -1,3 +1,16 @@
 var numberFormat = function (number) {
-  return number.toLocaleString()
+    return number.toLocaleString()
 };
+
+function reverseWords(str) {
+	const arr = str.split(" ")
+	let arr2 = []
+	for(let item of arr) {
+	  let result = item.split("").reverse().join("")
+	  arr2.push(result)
+	}
+	const r = arr2.join(' ')
+	return r
+	
+  }
+  console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
