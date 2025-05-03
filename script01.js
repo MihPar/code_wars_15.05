a544736e4acd7ff4869ad5e567745a55c22e30b4
MihPar <9496260@gmail.com>
2025-05-03 17:13:42 +0300

    next commit 3.04.2025

--- script01.js
+++ script01.js
@@ -112,3 +112,16 @@ function sumStr(a,b) {
   
 	return finalValue.toFixed(2);
   }
+
+  function folding(a, b) {
+	let count = 0;
+   
+	 while (b !== 0) {
+	   count += Math.floor(a / b);
+	   let temp = a % b;
+	   a = b;
+	   b = temp;
+	 }
+   
+	 return count;
+   }
